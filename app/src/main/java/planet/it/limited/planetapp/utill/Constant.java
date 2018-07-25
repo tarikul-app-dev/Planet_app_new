3e4b28fe48d14af05dd80aa5e1df948bd052b493
Tarikul <tarikul.android@gmail.com>
2018-07-25 16:24:14 +0600

    last update

--- app/src/main/java/planet/it/limited/planetapp/utill/Constant.java
+++ app/src/main/java/planet/it/limited/planetapp/utill/Constant.java
@@ -12,7 +12,7 @@ public class Constant {
     public static final String singleSMSAPI = "https://api.infobip.com/sms/1/text/single";
     public static final String bkashAPI = "http://apps.planetgroupbd.com/ords/accounts/planet/bkash?";
     public static final String checkUserAPI = "http://apps.planetgroupbd.com/ords/accounts/planet/users/";
-    public static final String serviesAPI = "https://apps.planetgroupbd.com/ords/f?p=PLANET_IT:SERVICES:::NO:::";
+
 
     Context mContext;
     public Constant(Context context){
